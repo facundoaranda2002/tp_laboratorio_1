cf460ff50007cf680b48289d5be1a76a7c3f9bca
facundoaranda2002 <facundoaranda67@gmail.com>
2022-04-04 08:28:50 -0300

    carpeta debug

--- TP_[1]/src/TP1.c
+++ TP_[1]/src/TP1.c
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	printf("!!!Hello World!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
